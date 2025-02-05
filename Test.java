9047e77cd7c034adeffde5beda4b0ef020d57b1a
Sathish-43 <20csa43@karpagamtech.ac.in>
2025-02-05 16:14:43 +0530

    refactor : small modification

--- Test.java
+++ Test.java
@@ -2,7 +2,6 @@ public class Test{
    public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Using Merge Conflit for Push Resquest");
-
-      System.out.println("Using Merge Conflit for Pull Request");
+        System.out.println("Using Merge Conflit for Pull Request");
     }
 }
